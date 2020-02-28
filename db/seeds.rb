c2526acf9b753b06ed9bc82c23384bc3fb11b367
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-02-28 11:48:32 +0800

    Gem update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+
+10.times do
+  ingredient_name = Faker::Food.ingredient
+  Ingredient.create(name: ingredient_name)
+end
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Negroni")
+Cocktail.create(name: "Spritz")
+Cocktail.create(name: "Moscow Mule")
+Cocktail.create(name: "Gin & Tonic")
+
+
+
